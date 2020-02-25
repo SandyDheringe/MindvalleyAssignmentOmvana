510fa7685b5886bfb21c3cb24b2d7c10a21abf39
Sandeep Dheringe <sandeep.dheringe@pebstone.net>
2020-02-25 23:56:00 +0530

    git ignore updated

---
+++ app/src/test/java/com/mindvalley/view/pinboard/PinboardRepositoryTest.kt
@@ -0,0 +1,25 @@
+package com.mindvalley.view.pinboard
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PinboardRepositoryTest
+{
+
+    @Before
+    fun setUp()
+    {
+    }
+
+    @Test
+    fun getUserDetails()
+    {
+    }
+
+    @Test
+    fun clearCache()
+    {
+    }
+}
